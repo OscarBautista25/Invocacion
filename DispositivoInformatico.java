22f5a20d91cc5a0e8309f4cdeca90128fe7d33bf
Oscar Bautista <odbautista@profesores.uniajc.edu.co>
2024-09-19 20:36:22 -0500

    Cambios en el repo de OscarBautista25

--- DispositivoInformatico.java
+++ DispositivoInformatico.java
@@ -4,6 +4,7 @@
 public class DispositivoInformatico {
 
     String marca = "MAC";
+    String referencia = "Sierra";
 
     public DispositivoInformatico() {
         System.out.println("Marca = " +marca);
